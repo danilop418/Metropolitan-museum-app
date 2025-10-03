021575b1f8e2be3bdf8e6a2af71eb810425c8b07
danilop418 <daniel.lopgon.4@educa.jcyl.es>
2025-10-03 07:26:30 +0200

    Commit : I created the architecture of design and I added the class, packages and interfaces.

---
+++ app/src/main/java/edu/iesam/metropolitan_museum/features/museum/data/MuseumDataRepository.kt
@@ -0,0 +1,4 @@
+package edu.iesam.metropolitan_museum.features.museum.data
+
+class MuseumDataRepository {
+}
---
+++ app/src/main/java/edu/iesam/metropolitan_museum/features/museum/domain/MuseumFetchUseCase.kt
@@ -0,0 +1,4 @@
+package edu.iesam.metropolitan_museum.features.museum.domain
+
+class MuseumFetchUseCase {
+}
