2a4020631dc830de4b76afbb2540fc4653602270
danilop418 <daniel.lopgon.4@educa.jcyl.es>
2025-10-03 08:53:34 +0200

    Commit: I changed all functions to suspend

--- app/src/main/java/edu/iesam/metropolitan_museum/features/museum/domain/MuseumFetchUseCase.kt
+++ app/src/main/java/edu/iesam/metropolitan_museum/features/museum/domain/MuseumFetchUseCase.kt
@@ -1,7 +1,7 @@
 package edu.iesam.metropolitan_museum.features.museum.domain
 
 class FetchSuperHeroeUseCase(val superHeroeRepository: MuseumRepository) {
-    fun fetch(): Result<List<WorkOfArt>> {
+    suspend fun fetch(): Result<List<WorkOfArt>> {
         return superHeroeRepository.fetch()
     }
 }
